2f2f484aab9a9e10b483628c0f96ffefda17fb53
Chirag5528 <chirag.arora5560@gmail.com>
2020-12-29 11:06:11 +0530

    Making the design responsive with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,13 @@ module.exports = {
       },
       fontFamily:{
         'nunito': ['Nunito']
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       }
     },
   },
